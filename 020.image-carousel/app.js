20c57a261faabd354bb84167e9a00e3f129c5dd1
Kone Issa <kissadev10@gmail.com>
2024-08-30 23:06:39 +0100

    Gallery Crousel

---
+++ 020.image-carousel/app.js
@@ -0,0 +1,18 @@
+const scrollGallery = document.getElementById("gallery");
+const back = document.getElementById("back");
+const next = document.getElementById("next");
+
+scrollGallery.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  scrollGallery.scrollLeft += e.deltaY;
+});
+
+back.addEventListener("click", () => {
+  scrollGallery.style.scrollBehavior = "smooth";
+  scrollGallery.scrollLeft -= 600;
+});
+
+next.addEventListener("click", () => {
+  scrollGallery.style.scrollBehavior = "smooth";
+  scrollGallery.scrollLeft += 600;
+});
